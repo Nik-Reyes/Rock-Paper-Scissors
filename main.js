390116a9c334680647077757c2ef42c7a4b07142
“Nik <nikolaspreyes@gmail.com>
2025-03-14 20:44:31 -0700

    fix: complete logic to play 1 match of r-p-s

--- main.js
+++ main.js
@@ -1,82 +1,27 @@
-// Breaking the problem down into smaller bits
+const humanScoreText = document.querySelector("#human-score");
+const compScoreText = document.querySelector("#computer-score");
+const drawText = document.querySelector("#draw-count");
+let roundCount = document.querySelector("#round-count");
+const playButton = document.querySelector(".play-button");
+const playBox = document.querySelector(".hidden");
+const content = document.querySelector(".content");
+const resultContainer = document.querySelector("#result-container");
+const resultDisplay = document.querySelector("#result-display");
 
-// 1.) Create a get function that returns a random number 1-3. THis is the computers choice
-// 2.) Create a get function that returns the users human players choice of rock, paper, or scissors
-// 3.) Users choice is a word, but computers choice is a number. Create a function that converts the humans choice into a number
-// 4.) Human choice and computer choice are now comparible. Create a function that
-
-//write a function that takes the users choice of rock paper or scissors (use prompt method)
-//to make sure that the user enters a correct choice and to account for incorrect input, use a while or do while loop. for example, while the input is !== to correct input, call function to prompt again.
-
-//increment the player scores so that the game can end
-
-//whoever reaches the score of 3 first, wins
-
-let roundCount = 1;
-let humanScore = 0;
-let computerScore = 0;
-let computerChoice;
-let humanChoice;
-
-function printChoices(human, computer) {
-  let humanChoiceToText;
-  let computerChoiceToText;
-
-  switch (computer) {
-    case 1:
-      computerChoiceToText = "rock";
-      break;
-    case 2:
-      computerChoiceToText = "paper";
-      break;
-    case 3:
-      computerChoiceToText = "scissors";
-      break;
+const roundEval = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    return 3;
   }
 
-  switch (human) {
-    case 1:
-      humanChoiceToText = "rock";
-      break;
-    case 2:
-      humanChoiceToText = "paper";
-      break;
-    case 3:
-      humanChoiceToText = "scissors";
-      break;
-  }
-
-  console.log(`You: ${humanChoiceToText}\nComputer: ${computerChoiceToText}`);
-}
-
-function printScore(human, computer) {
-  console.log(`You: ${human}, Computer: ${computer}`);
-}
-
-function evalGame(humanScore, computerScore) {
-  if (humanScore === computerScore) {
-    console.log("It's a draw!");
-    alert("It's a draw!");
-  } else if (humanScore > computerScore) {
-    console.log("You win!");
-    alert("You win!");
+  if (
+    (humanChoice === 1 && computerChoice === 3) ||
+    (humanChoice === 2 && computerChoice === 1) ||
+    (humanChoice === 3 && computerChoice === 2)
+  ) {
+    return 1;
   } else {
-    console.log("The computer wins!");
-    alert("The computer wins!");
+    return 2;
   }
-}
-
-const getComputerChoice = () => {
-  return Math.floor(Math.random() * 3) + 1;
-};
-
-const getHumanChoice = () => {
-  let choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-  while (!(choice === "rock" || choice === "paper" || choice === "scissors")) {
-    alert(`${choice} is not a valid entry`);
-    choice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-  }
-  return convertHumanChoiceToNumber(choice);
 };
 
 function convertHumanChoiceToNumber(str) {
@@ -90,48 +35,91 @@ function convertHumanChoiceToNumber(str) {
   }
 }
 
-const roundEval = (humanChoice, computerChoice) => {
-  printChoices(humanChoice, computerChoice);
-  if (humanChoice === computerChoice) {
-    console.log("It's a draw!");
-    console.log(`\n`);
+const getComputerChoice = () => {
+  return Math.floor(Math.random() * 3) + 1;
+};
 
+function getHeaderNumber(header, delimeter) {
+  const t = header.innerText.split(delimeter);
+  return parseInt(t.pop());
+}
+
+function updateRound() {
+  console.log(getHeaderNumber(roundCount, "#"));
+  if (getHeaderNumber(roundCount, "#") >= 5) {
+    roundCount.innerText = `Game Over!`;
     return;
   }
+  roundCount.innerText = `Round #${getHeaderNumber(roundCount, "#") + 1}`;
+}
 
-  if (
-    (humanChoice === 1 && computerChoice === 3) ||
-    (humanChoice === 2 && computerChoice === 1) ||
-    (humanChoice === 3 && computerChoice === 2)
-  ) {
-    console.log("You win this round!");
-    console.log(`\n`);
-    humanScore++;
-  } else {
-    console.log("Computer wins this round!");
-    console.log(`\n`);
-    computerScore++;
+function updateScoreBox(result) {
+  switch (result) {
+    case 1:
+      humanScoreText.innerText = `You: ${
+        getHeaderNumber(humanScoreText, ":") + 1
+      }`;
+      break;
+    case 2:
+      compScoreText.innerText = `Computer: ${
+        getHeaderNumber(compScoreText, ":") + 1
+      }`;
+      break;
+    case 3:
+      drawText.innerText = `Draws: ${getHeaderNumber(drawText, ":") + 1}`;
+      break;
+  }
+}
+
+function displayResult() {
+  const humanScore = getHeaderNumber(humanScoreText, ":");
+  const computerScore = getHeaderNumber(compScoreText, ":");
+  const draws = getHeaderNumber(drawText, ":");
+  resultContainer.classList.remove("hidden");
+
+  if (humanScore > computerScore) {
+    resultDisplay.innerText = "You won!";
+  } else if (computerScore > humanScore) {
+    resultDisplay.innerText = "The computer won!";
+  } else if (computerScore === humanScore || draws === 5) {
+    resultDisplay.innerText = "It's a draw!";
+  }
+}
+
+const handleButtonClick = (e) => {
+  if (e.target.closest(".btn") !== null) {
+    humanChoice = e.target.closest(".btn").innerText.toLowerCase().trim();
+
+    const roundResult = roundEval(
+      convertHumanChoiceToNumber(humanChoice),
+      getComputerChoice()
+    );
+    if (roundResult === 1 || roundResult === 2 || roundResult === 3) {
+      if (getHeaderNumber(roundCount, "#") <= 6) {
+        updateRound();
+      }
+      updateScoreBox(roundResult);
+      if (
+        getHeaderNumber(humanScoreText, ":") >= 3 ||
+        getHeaderNumber(compScoreText, ":") >= 3 ||
+        getHeaderNumber(roundCount, "#") >= 5 ||
+        (getHeaderNumber(drawText, ":") >= 3 &&
+          getHeaderNumber(humanScoreText, ":") >= 1) ||
+        (getHeaderNumber(drawText, ":") >= 3 &&
+          getHeaderNumber(compScoreText, ":") >= 1)
+      ) {
+        buttons.removeEventListener("click", handleButtonClick);
+        displayResult();
+      }
+    }
   }
 };
 
-function gameStart() {
-  do {
-    //get computer choice
-    computerChoice = getComputerChoice();
-
-    //get human choice
-    humanChoice = getHumanChoice();
-
-    //evaluate if the human or the computer won
-    roundEval(humanChoice, computerChoice);
-    roundCount++;
-  } while (humanScore < 3 && computerScore < 3 && roundCount <= 5);
-}
-
-function runApp() {
-  gameStart();
-  printScore(humanScore, computerScore);
-  evalGame(humanScore, computerScore);
-}
-
-runApp();
+const buttons = document.querySelector(".game-buttons");
+buttons.addEventListener("click", handleButtonClick);
+playButton.addEventListener("click", () => {
+  playButton.remove();
+  playBox.classList.remove("hidden");
+  playBox.classList.add("fade-in");
+  content.classList.add("fade-in");
+});
